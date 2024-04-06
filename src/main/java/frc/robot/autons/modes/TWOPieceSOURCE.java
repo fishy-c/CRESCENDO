a0f16c9e3395a3c49c5d3c1699bdd2773d47927e
josie-miller <144562888+josie-miller@users.noreply.github.com>
2024-04-06 01:05:13 -0700

    individual commits (sorry)

--- src/main/java/frc/robot/autons/modes/TWOPieceSOURCE.java
+++ src/main/java/frc/robot/autons/modes/TWOPieceSOURCE.java
@@ -12,29 +12,31 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Subsystems.Superstructure;
 import frc.robot.Subsystems.Superstructure.SuperstructureStates;
 import frc.robot.Subsystems.Swerve.Swerve;
 
-public class TwoPieceSource extends SequentialCommandGroup{
-    private final PathPlannerPath MidPath = PathPlannerPath.fromChoreoTrajectory("sourceclose");
-    public TwoPieceSource(Swerve swerve, Superstructure superstructure){
-        SuperstructureStates shootSide = DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Blue ? SuperstructureStates.SPIN_UP_RIGHT : SuperstructureStates.SPIN_UP_RIGHT;
+public class TWOPieceSOURCE extends SequentialCommandGroup{
+    private final PathPlannerPath SourcePathForward = PathPlannerPath.fromChoreoTrajectory("sourcecloseforward");
+    private final PathPlannerPath SourcePathBackward = PathPlannerPath.fromChoreoTrajectory("sourceclosebackward");
+    public TWOPieceSOURCE(Swerve swerve, Superstructure superstructure){
+    SuperstructureStates shootSide = DriverStation.getAlliance().equals(Alliance.Blue) ? SuperstructureStates.SPIN_UP_RIGHT : SuperstructureStates.SPIN_UP_RIGHT;
         addRequirements(swerve, superstructure);
         addCommands(
-            new InstantCommand(() -> swerve.resetPose(new Pose2d(new Translation2d(0.6724225282669067, 4.376314640045166), new Rotation2d(-1.0461863225243422)))),
+            new InstantCommand(() -> swerve.resetPose(new Pose2d(new Translation2d(0.6709336638450623,4.377834320068359), new Rotation2d(-1.0471975512)))),
             new InstantCommand(() -> superstructure.setState(shootSide)),
-            new WaitCommand(2),
+            new WaitCommand(1.25),
             new ParallelCommandGroup(
-            AutoBuilder.followPath(MidPath),
+            AutoBuilder.followPath(SourcePathForward),
             new SequentialCommandGroup(
                 new WaitCommand(0.7),
                 new InstantCommand(() -> superstructure.setState(SuperstructureStates.INTAKE)))
                 ),
+            new WaitCommand(0.3),
+            AutoBuilder.followPath(SourcePathBackward),
             new ParallelCommandGroup(
-                new WaitCommand(2),
+                new WaitCommand(1.25),
                 new InstantCommand(() -> superstructure.setState(shootSide))
             )
             );
