87613e31d469fbb309f4cfddf1938928ee9fafe3
josie-miller <144562888+josie-miller@users.noreply.github.com>
2024-04-06 01:05:36 -0700

    individual commits (sorry)

--- src/main/java/frc/robot/autons/AutonomousSelector.java
+++ src/main/java/frc/robot/autons/AutonomousSelector.java
@@ -5,41 +5,35 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Subsystems.Superstructure;
 import frc.robot.Subsystems.Swerve.Swerve;
-import frc.robot.autons.modes.Hardstop;
-import frc.robot.autons.modes.TWOPIECE_MID;
-import frc.robot.autons.modes.PRELOAD_LEAVE_AMP;
-import frc.robot.autons.modes.PRELOAD_LEAVE_MID;
-import frc.robot.autons.modes.PRELOAD_LEAVE_SOURCE;
-import frc.robot.autons.modes.PRELOAD_AMP;
-import frc.robot.autons.modes.PRELOAD_MID;
-import frc.robot.autons.modes.PRELOAD_SOURCE;
-
-import frc.robot.autons.modes.TwoPieceMid;
+import frc.robot.autons.modes.TWOPieceMid;
+import frc.robot.autons.modes.TWOPieceAMP;
+import frc.robot.autons.modes.TWOPieceSOURCE; //E for Source
+import frc.robot.autons.modes.EFarMID;
+import frc.robot.autons.modes.EFarAMP;
+import frc.robot.autons.modes.EFarMIDTHREE; //EFarAMP with TWOPieceMid
+import frc.robot.autons.modes.THREEPieceAMP;
+import frc.robot.autons.modes.THREEPieceMid;
 
 public class AutonomousSelector {
     private SendableChooser<SequentialCommandGroup> autonomousSelector = new SendableChooser<SequentialCommandGroup>();
 
     public AutonomousSelector(Swerve swerve, Superstructure superstructure){
         autonomousSelector.setDefaultOption(
-            "test", new TwoPieceMid(swerve, superstructure));
+            "testTWOPieceMid", new TWOPieceMid(swerve, superstructure));
+       
+        autonomousSelector.addOption("testTWOPieceAMP", new TWOPieceAMP(swerve, superstructure));
         
-        autonomousSelector.addOption("PRELOAD_AMP", new PRELOAD_AMP(swerve, superstructure));
+        autonomousSelector.addOption("testTWOPieceSOURCE", new TWOPieceSOURCE(swerve, superstructure));
 
-        autonomousSelector.addOption("PRELOAD_SOURCE", new PRELOAD_SOURCE(swerve, superstructure));
+        autonomousSelector.addOption("testEFarMID", new EFarMID(swerve, superstructure));
 
-        autonomousSelector.addOption("TWOPIECE_MID", new TWOPIECE_MID(swerve, superstructure));
+        autonomousSelector.addOption("testEFarAMP", new EFarAMP(swerve, superstructure));
 
-        autonomousSelector.addOption("PRELOAD_LEAVE_MID", new PRELOAD_LEAVE_MID(swerve, superstructure));
+        autonomousSelector.addOption("testEFarMIDTHREE", new EFarMIDTHREE(swerve, superstructure));
 
-        autonomousSelector.addOption("PRELOAD_LEAVE_AMP", new PRELOAD_LEAVE_AMP(swerve, superstructure));
-        
-        autonomousSelector.addOption("PRE_LOAD_LEAVE_SOURCE", new PRELOAD_LEAVE_SOURCE(swerve, superstructure));
+        autonomousSelector.addOption("THREEPieceAMP", new THREEPieceAMP(swerve, superstructure));
 
-        //autonomousSelector.addOption("testHardstop", new Hardstop(swerve, superstructure));
-
-
-        autonomousSelector.addOption("PRELOAD_SOURCE", new PRELOAD_SOURCE(swerve, superstructure));
-        
+        autonomousSelector.addOption("THREEPieceMid", new THREEPieceMid(swerve, superstructure));
 
         SmartDashboard.putData("Auto Choices", autonomousSelector);
 
