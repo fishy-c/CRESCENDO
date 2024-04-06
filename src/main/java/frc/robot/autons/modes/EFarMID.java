e0e194445d96e8777810fae91195ede358e2eeb5
josie-miller <144562888+josie-miller@users.noreply.github.com>
2024-04-06 01:02:51 -0700

    individual commits (sorry)

---
+++ src/main/java/frc/robot/autons/modes/EFarMID.java
@@ -0,0 +1,43 @@
+package frc.robot.autons.modes;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Subsystems.Superstructure;
+import frc.robot.Subsystems.Superstructure.SuperstructureStates;
+import frc.robot.Subsystems.Swerve.Swerve;
+
+public class EFarMID extends SequentialCommandGroup{
+    private final PathPlannerPath MidPathForward = PathPlannerPath.fromChoreoTrajectory("Emidfarforward");
+    private final PathPlannerPath MidPathBackward = PathPlannerPath.fromChoreoTrajectory("Emidfarbackward");
+    public EFarMID(Swerve swerve, Superstructure superstructure){
+        addRequirements(swerve, superstructure);
+        addCommands(
+            new InstantCommand(() -> swerve.resetPose(new Pose2d(new Translation2d(1.3642183542251587, 5.5551371574401855), new Rotation2d(0)))),
+            new InstantCommand(() -> superstructure.setState(SuperstructureStates.SPIN_UP_MID)),
+            new WaitCommand(1.25),
+            new ParallelCommandGroup(
+            AutoBuilder.followPath(MidPathForward),
+            new SequentialCommandGroup(
+                new WaitCommand(1.9),
+                new InstantCommand(() -> superstructure.setState(SuperstructureStates.INTAKE)))
+                ),
+            new WaitCommand(0.3),
+            AutoBuilder.followPath(MidPathBackward),
+            new ParallelCommandGroup(
+                new WaitCommand(1.25),
+                new InstantCommand(() -> superstructure.setState(SuperstructureStates.SHOOT_MID))
+            )
+            );
+                    
+    }
+}
